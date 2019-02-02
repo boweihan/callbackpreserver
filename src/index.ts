adcb8027be33362d2ce2b504e20e31b8fc5e8d1f
bowei <bowei.han100@gmail.com>
2019-02-02 15:12:19 -0500

    close()

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,48 @@
-export const Test = () => `Hello`;
+import { partial } from "lodash";
 
 export default class Transformer {
-  private args: any[];
-  private executor: IterableIterator<any>;
-  private next: (...args: any[]) => Promise<any>;
+  private args?: any[];
+  private executor?: IterableIterator<any>;
+  private next?: (...args: any[]) => any;
 
-  public create(source: (...args: any[]) => Promise<void>) {
+  public create = (source: (...args: any[]) => Promise<void>) => {
     let resolver: () => void;
     const status = new Promise(resolve => {
       resolver = resolve;
     });
     source((...args: any[]) => this.init(args, resolver));
     return status;
-  }
-
-  public run = (callable: Function): Promise<any> => {
-    this.next = _.partial(callable, this.ctx);
-    try {
-      let returnValue = this.worker && this.worker.next(false).value;
-      return Promise.resolve(returnValue);
-    } catch (e) {
-      return Promise.reject(e);
-    }
   };
 
-  private init(args: any[], resolver: () => void): Promise<any> {
+  public close = () => {
+    if (this.executor) {
+      this.executor.next(true);
+    }
+    delete this.next;
+    delete this.args;
+    delete this.executor;
+  };
+
+  public run = (
+    callable: (...args: any[]) => Promise<any>,
+  ): Promise<any> | Promise<never> => {
+    this.next = partial(callable, this.args);
+    let result: Promise<any> | Promise<never>;
+    try {
+      const val = this.executor.next(false).value;
+      result = Promise.resolve(val);
+    } catch (e) {
+      result = Promise.reject(e);
+    }
+    return result;
+  };
+
+  private init = (args: any[], resolver: () => void): Promise<any> => {
     this.args = args;
     this.executor = this.execute();
     resolver();
     return Promise.resolve();
-  }
+  };
 
   private *execute() {
     let close = false;
