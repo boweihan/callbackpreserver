3df0beab381fca1c5a0a18eb6e41846e22832737
bowei <bowei.han100@gmail.com>
2019-02-02 14:50:29 -0500

    create()

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,5 +1,22 @@
-import { Test } from "../index";
+import Transformer, { Test } from "../index";
 
 test("Test", () => {
   expect(Test()).toBe("Hello");
 });
+
+// let source = context => {
+//   // do things
+// };
+//
+// let t = new Transformer(source);
+// t.execute(context => {
+//   //do things
+// });
+// t.execute(context => {
+//   // do things
+// });
+// t.close();
+//
+//
+//
+// let source = ExcelService.run((ctx) => {})
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
 export const Test = () => `Hello`;
 
 export default class Transformer {
-  private create = async (callable: () => any) => {
-    let completed = () => {};
+  private args: any[];
+  private executor: IterableIterator<any>;
+  private next: (...args: any[]) => Promise<any>;
+
+  public create(source: (...args: any[]) => Promise<void>) {
+    let resolver: () => void;
     const status = new Promise(resolve => {
-      completed = resolve;
+      resolver = resolve;
     });
+    source((...args: any[]) => this.init(args, resolver));
     return status;
+  }
+
+  public run = (callable: Function): Promise<any> => {
+    this.next = _.partial(callable, this.ctx);
+    try {
+      let returnValue = this.worker && this.worker.next(false).value;
+      return Promise.resolve(returnValue);
+    } catch (e) {
+      return Promise.reject(e);
+    }
   };
+
+  private init(args: any[], resolver: () => void): Promise<any> {
+    this.args = args;
+    this.executor = this.execute();
+    resolver();
+    return Promise.resolve();
+  }
+
+  private *execute() {
+    let close = false;
+    while (!close) {
+      close = yield this.next && this.next();
+    }
+  }
 }
