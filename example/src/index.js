05b31859912d821db9c074884d68e3ba5625be30
bowei <bowei.han100@gmail.com>
2019-02-09 13:24:07 -0500

    example

--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'babel-polyfill';
 import CallbackPreserver from 'callbackpreserver';
-console.log(CallbackPreserver);
 
 let ctx;
 let preserver = new CallbackPreserver();
@@ -13,13 +12,15 @@ const addWorksheet = context => {
   var sheet = sheets.add(String(new Date().getTime()));
   sheet.load('name, position');
 
-  return context.sync().then(function() {
+  return context.sync().then(() => {
     console.log(
       `Added worksheet named "${sheet.name}" in position ${sheet.position}`,
     );
   });
 };
 
+// retain context or preserve, preserving preserves closure context whereas
+// maintaining a reference to the context does not!
 window.Office.initialize = () => {
   render();
   window.Excel.run(context => {
@@ -29,14 +30,21 @@ window.Office.initialize = () => {
   preserver.preserve(window.Excel.run);
 };
 
+// two different ways to use use context
 let render = () => {
   ReactDOM.render(
-    <button onClick={() => addWorksheet(ctx)}>
-      Add Worksheet (Callbacks)
-    </button>,
-    <button onClick={() => preserver.run(context => addWorksheet(context))}>
-      Add Worksheet (Preserver)
-    </button>,
+    <div>
+      <button onClick={() => addWorksheet(ctx)}>
+        Add Worksheet (Callbacks)
+      </button>
+      <button
+        onClick={() => {
+          preserver.run(context => addWorksheet(context));
+        }}
+      >
+        Add Worksheet (Preserver)
+      </button>
+    </div>,
     document.getElementById('root'),
   );
 };
